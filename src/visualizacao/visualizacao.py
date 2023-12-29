8addde0f047d127838c2395f4ae17293896f271c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-12-29 17:30:43 -0300

    método para agrupar o gráfico

--- src/visualizacao/visualizacao.py
+++ src/visualizacao/visualizacao.py
@@ -1,1 +1,31 @@
-print("versão do streamlit")
+import pandas as pd
+import plotly.express as px
+
+
+class Visualizacao:
+    def __init__(self, dataframe: pd.DataFrame) -> None:
+        self.__dataframe = dataframe
+
+    def gerar_visualizacao_grafico_barra(
+        self, coluna_x: str, coluna_y: str, color: str = None, barmode: str = None
+    ):
+        """Método para gerar o gráfico de barras agrupado
+
+        Args:
+            coluna_x (str): coluna X do df
+            coluna_y (str): coluna y
+            color (str, optional): atributo para cor. Defaults to None.
+            barmode (str, optional): agrupamento do gráfico. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        fig = px.bar(
+            self.__dataframe,
+            x=coluna_x,
+            y=coluna_y,
+            color=color,
+            barmode=barmode,
+            text_auto="0",
+        )
+        return fig
