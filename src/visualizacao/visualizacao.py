e9a79f6a5d9b5e65f192f7fa0eb68f8b0f67ec12
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-12-29 19:10:50 -0300

    configurações de cor

--- src/visualizacao/visualizacao.py
+++ src/visualizacao/visualizacao.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from typing import List
 
 
 class Visualizacao:
@@ -7,18 +9,26 @@ class Visualizacao:
         self.__dataframe = dataframe
 
     def gerar_visualizacao_grafico_barra(
-        self, coluna_x: str, coluna_y: str, color: str = None, barmode: str = None
+        self,
+        coluna_x: str,
+        coluna_y: str,
+        cor_sequencia_legenda: List[str],
+        titulo_grafico: str = "Teste",
+        color: str = None,
+        barmode: str = None,
     ):
-        """Método para gerar o gráfico de barras agrupado
+        """Método para gerar gráfio de barra
 
         Args:
-            coluna_x (str): coluna X do df
-            coluna_y (str): coluna y
-            color (str, optional): atributo para cor. Defaults to None.
-            barmode (str, optional): agrupamento do gráfico. Defaults to None.
+            coluna_x (str): eixo x do gráfico
+            coluna_y (str): eixo y do gráfico
+            cor_sequencia_legenda (List[str]): cor das legendas
+            titulo_grafico (str, optional): título do grafico. Defaults to "Teste".
+            color (str, optional): cor para separação no gráfico. Defaults to None.
+            barmode (str, optional): coluna de agrupamento. Defaults to None.
 
         Returns:
-            _type_: _description_
+            _type_: figura
         """
         fig = px.bar(
             self.__dataframe,
@@ -27,5 +37,16 @@ class Visualizacao:
             color=color,
             barmode=barmode,
             text_auto="0",
+            color_discrete_sequence=cor_sequencia_legenda,
         )
+        fig.update_layout(
+            title_text=titulo_grafico,
+            showlegend=True,
+            title=dict(x=0.5, font=dict(color="white", size=12)),
+            # plotly_bgcolor=None,
+            xaxis=dict(title="", tickfont=dict(color="white")),
+            legend=dict(font=dict(color="white")),
+        )
+
+        fig.update_traces(textfont_color="white")
         return fig
